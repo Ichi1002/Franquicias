55531c444af3e42f087dc14980834c84f591d04c
Leo <carleo1005@gmail.com>
2025-02-18 15:42:27 -0500

    Add usecases

---
+++ src/main/java/com/franquicia/domain/usecase/FranchiseUseCase.java
@@ -0,0 +1,18 @@
+package com.franquicia.domain.usecase;
+
+import com.franquicia.domain.models.Franchise;
+import com.franquicia.domain.models.Product;
+import com.franquicia.domain.models.Subsidiary;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FranchiseUseCase {
+    Franchise createFranchise(Franchise franchise);
+    Franchise addSubsuduaryToFranchise(String franchiseName, Subsidiary subsidiary);
+    Subsidiary addProductToSubsidiary(String subsidiaryName, Product product);
+    Subsidiary deleteProductOfSubsidiary(String subsidiaryName, String productName);
+    Subsidiary modifyProductStockOfSubsidiary(String subsidiaryName, Product product);
+    List<Map<String,Product>> getBiggerStickProductInSubsidiaryByFranchise(String franchiseName);
+
+}
